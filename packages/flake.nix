d97ef976a857c8c5bd0b94d60ddc4c142bddeb60
Alex <alex@arekisannda.dev>
2026-03-27 00:23:43 -0700

    add packages flake

---
+++ packages/flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "Custom Packages";
+
+  inputs = {
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs =
+    inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "aarch64-darwin"
+        "x86_64-darwin"
+      ];
+
+      perSystem =
+        { pkgs, ... }:
+        {
+          packages.swayrst = pkgs.callPackage ./swayrst.nix { };
+          packages.rass = pkgs.callPackage ./rass.nix { };
+        };
+    };
+}
