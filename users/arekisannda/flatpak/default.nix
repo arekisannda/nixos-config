6640d53472e4e12318789fd920039e2d5ba463ee
Alex <alex@arekisannda.dev>
2026-07-22 18:12:12 -0700

    update configurations

--- users/arekisannda/flatpak/default.nix
+++ users/arekisannda/flatpak/default.nix
@@ -3,5 +3,10 @@
   services.flatpak.enable = true;
   services.flatpak.uninstallUnmanaged = true;
 
-  services.flatpak.packages = [ ];
+  services.flatpak.packages = [
+    {
+      appId = "com.usebottles.bottles";
+      commit = "53692c06f12cc0ded97f7ecd32d5e414177f2416";
+    }
+  ];
 }
